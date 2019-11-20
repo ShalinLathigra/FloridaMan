13d1044a420c6658f7ec4ed0076d66006f4ed05c
David Nguyen <davidnguyen3@WINDOWS2.SCS.CARLETON.CA>
2019-11-20 17:36:47 -0500

    Rebased, some of my changes were removed. Will add them back.

---
+++ FloridaMan/laser_node.cpp
@@ -0,0 +1,107 @@
+#include "laser_node.h"
+#include <iostream>
+#include <glm/gtc/matrix_transform.hpp>
+
+namespace game
+{
+	LaserNode::LaserNode(const std::string name, const Resource *geometry, const Resource *material) : SceneNode(name, geometry, material)
+	{
+		active_ = false;
+		std::cout << "This is the laser on creation immediate: " << active_ << std::endl;
+		width_ = .5f;
+		forward_ = glm::vec3(0.0f, 0.0f, 1.0f);	//initial forward
+
+		max_duration_timer_ = .25f;
+		max_cd_timer_ = .25f;
+
+		duration_timer_ = 0.0f;
+		cd_timer_ = 0.0f;
+
+		joint_ = glm::vec3(0.0f);
+		orbit_amount_ = glm::angleAxis(glm::pi<float>() / 2.0f, glm::vec3(-1.0f, 0.0f, 0.0f));
+	}
+
+	LaserNode::~LaserNode()
+	{
+	}
+
+	void LaserNode::turnOff(void) {
+		active_ = false;
+	}
+
+	void LaserNode::Draw(Camera *camera)
+	{
+		if (active_ == true) {
+			SceneNode::Draw(camera);
+		}
+		else
+			return;
+	}
+	void LaserNode::Update(float deltaTime)
+	{
+		AdvanceTimers(deltaTime);
+	}
+
+	//Advance Cooldowns and Laser Duration
+	void LaserNode::AdvanceTimers(float deltaTime)
+	{
+		if (duration_timer_ > 0.0f)
+		{
+			duration_timer_ = std::max(0.0f, duration_timer_ - deltaTime);
+		}
+		else if (cd_timer_ > 0.0f && duration_timer_ == 0.0f)
+		{
+			cd_timer_ = std::max(0.0f, cd_timer_ - deltaTime);
+		}
+
+		if (duration_timer_ == 0.0f)
+			active_ = false;
+	}
+	void LaserNode::Fire(void)
+	{
+		if (cd_timer_ == 0.0f && active_ == false)
+		{
+			duration_timer_ = max_duration_timer_;
+			cd_timer_ = max_cd_timer_;
+			active_ = true;
+
+			GetForward();
+		}
+		else if (cd_timer_ == 0.0f && active_ == true) {
+			active_ = false;
+		}
+	}
+	bool LaserNode::IsActive(void)
+	{
+		return active_;
+	}
+
+	float LaserNode::GetWidth(void) const
+	{
+		return width_;
+	}
+
+	glm::vec3 LaserNode::GetForward(void) const
+	{
+		return forward_; // Return -forward since the camera coordinate system points in the opposite direction
+	}
+
+	void LaserNode::SetJoint(glm::vec3 joint)
+	{
+		joint_ = joint;
+	}
+
+	glm::mat4 LaserNode::CalculateMatrix(glm::mat4 p)
+	{
+		glm::mat4 scaling = glm::scale(glm::mat4(1.0), scale_);
+		glm::mat4 rotation = glm::mat4_cast(orientation_);
+		glm::mat4 translation = glm::translate(glm::mat4(1.0), position_);
+
+		glm::mat4 orbit_rotation = glm::mat4_cast(orbit_amount_);
+		glm::mat4 orbit_translation = glm::translate(glm::mat4(1.0), -joint_);
+
+		glm::mat4 matrix_ = p * translation * (orbit_rotation * orbit_translation) * rotation;
+
+		return matrix_ * scaling;
+	}
+}
---
+++ FloridaMan/laser_node.h
@@ -0,0 +1,59 @@
+
+#ifndef LASER_H_
+#define LASER_H_
+
+#include <string>
+#define GLEW_STATIC
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#define GLM_FORCE_RADIANS
+#include <glm/gtc/quaternion.hpp>
+
+#include "resource.h"
+#include "scene_node.h"
+
+#include <algorithm>    // std::max
+
+namespace game
+{
+	class LaserNode :
+		public SceneNode
+	{
+	public:
+		LaserNode(const std::string name, const Resource *geometry, const Resource *material);
+		~LaserNode();
+
+		void Draw(Camera *camera) override;
+		void Update(float deltaTime);
+		void AdvanceTimers(float deltaTime);
+
+		void Fire(void);
+		void StartCooldown(void);
+		bool IsActive(void);
+
+		void turnOff(void);
+
+		float GetWidth(void) const;
+		glm::vec3 GetForward(void) const;
+		void SetJoint(glm::vec3);
+
+		glm::mat4 CalculateMatrix(glm::mat4 p);
+	private:
+		bool active_;
+
+		float width_;
+		glm::vec3 forward_;	//initial up
+
+		glm::vec3 joint_;
+		glm::quat orbit_amount_;
+
+		float max_duration_timer_;
+		float max_cd_timer_;
+
+		float duration_timer_;
+		float cd_timer_;
+	};
+}
+
+#endif // ASTEROID_H_
